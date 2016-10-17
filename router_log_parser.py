8b03fc3cd3e594c7339d89306ab3d63bf3c0a8e1
Rich O'Hara <richard@chimerasystems.net>
2016-10-16 20:49:38 -0400

    Added check to create log directory

--- router_log_parser.py
+++ router_log_parser.py
@@ -176,7 +176,7 @@ def main():
 logger_path = '/var/log/router_log_parser/logs'
 
 try:
-    os.makedirs(logger_path,"777")
+    os.makedirs(logger_path, 777)
 except OSError as exc:
         if os.path.isdir(logger_path):
             pass
